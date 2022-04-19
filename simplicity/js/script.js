a39324f4a1f17f212613c0acdf82b6b854d1f0c6
João Pedro <pedroganacim@gmail.com>
2022-04-19 17:03:07 -0300

    usando API FormSpree

--- simplicity/js/script.js
+++ simplicity/js/script.js
@@ -80,3 +80,36 @@ if (contagem == 0) {
 }
 
 });
+
+/* Progrmaçao do FormSpree */
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Obrigado por Enviar";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Eita! Deu Ruim ! Tente novamente mais tarde "
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
